ddcf18e5b408c0b0a8f5e1bb3f1c8119f9e5eeb3
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-03-02 23:19:44 +0000

    Simplified Board markup a bit

--- client/react/components/Board.js
+++ client/react/components/Board.js
@@ -10,26 +10,25 @@ const Board = ({
     active
 }) => {
     const highlights = outcomeToHighlights(outcome, winningLine);
+    const cells = indices =>
+        indices.map(idx =>
+            <td key={idx}>
+                <Cell
+                    value={board[idx]}
+                    active={active}
+                    highlight={highlights[idx]}
+                    onClick={() => onCellClick(idx)}
+                />
+            </td>
+        );
     return (
         <div>
             <h1>Tic-Tac-Toe</h1>
             <table id="board">
                 <tbody>
-                    <tr>
-                        <td><Cell value={board[0]} active={active} highlight={highlights[0]} onClick={() => onCellClick(0)} /></td>
-                        <td><Cell value={board[1]} active={active} highlight={highlights[1]} onClick={() => onCellClick(1)} /></td>
-                        <td><Cell value={board[2]} active={active} highlight={highlights[2]} onClick={() => onCellClick(2)} /></td>
-                    </tr>
-                    <tr>
-                        <td><Cell value={board[3]} active={active} highlight={highlights[3]} onClick={() => onCellClick(3)} /></td>
-                        <td><Cell value={board[4]} active={active} highlight={highlights[4]} onClick={() => onCellClick(4)} /></td>
-                        <td><Cell value={board[5]} active={active} highlight={highlights[5]} onClick={() => onCellClick(5)} /></td>
-                    </tr>
-                    <tr>
-                        <td><Cell value={board[6]} active={active} highlight={highlights[6]} onClick={() => onCellClick(6)} /></td>
-                        <td><Cell value={board[7]} active={active} highlight={highlights[7]} onClick={() => onCellClick(7)} /></td>
-                        <td><Cell value={board[8]} active={active} highlight={highlights[8]} onClick={() => onCellClick(8)} /></td>
-                    </tr>
+                    <tr>{cells([0, 1, 2])}</tr>
+                    <tr>{cells([3, 4, 5])}</tr>
+                    <tr>{cells([6, 7, 8])}</tr>
                 </tbody>
             </table>
         </div>
